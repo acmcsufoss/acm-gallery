f89b44ee67166839d54a7f8cd7eb170b0f1671f5
Tyler <tylerlui@gmail.com>
2024-09-24 23:51:38 -0700

    SvelteKit boilerplate

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<body class = "main-body">
+    <h1>ACM Gallery</h1>
+    <p>Gallery for all of ACM's pictures taken throughout the semester!</p>
+</body>
+
+
+<style>
+    @import '../styles.css';
+</style>
